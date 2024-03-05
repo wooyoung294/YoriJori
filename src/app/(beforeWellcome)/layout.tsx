9e8d48d16d66b6a05c18b3919d454bb313a2e074
wooyoung <andy241822@gmail.com>
2024-03-06 00:57:22 +0900

    [Feat]BeforeWellcome layout.tsx page.tsx

---
+++ src/app/(beforeWellcome)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from '@/app/(beforeWellcome)/layout.module.css'
+export default function Layout({children,}: { children: React.ReactNode }) {
+    return (
+        <div className={styles.container}>
+            {children}
+        </div>
+    );
+}
---
+++ src/app/(beforeWellcome)/page.tsx
@@ -0,0 +1,11 @@
+import styles from '@/app/(beforeWellcome)/page.module.css'
+
+export default function Home() {
+    return (
+        <main className={styles.container}>
+            <div>
+                <button>Search Recipes!</button>
+            </div>
+        </main>
+    );
+}
