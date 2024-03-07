9c8764d46174df0758cdcbb7b156a908a23a8b4a
wooyoung <andy241822@gmail.com>
2024-03-07 20:40:52 +0900

    [Update] 메인 화면 로고 추가

--- src/app/(afterWellcome)/layout.tsx
+++ src/app/(afterWellcome)/layout.tsx
@@ -1,36 +1,43 @@
 import React from "react";
 import styles from '@/app/(afterWellcome)/layout.module.css';
-import OrderListItem from "@/app/(afterWellcome)/_components/OrderListItem";
 import all from "../../../public/recipetype/all.png";
 import main from "../../../public/recipetype/main.png";
 import sub from "../../../public/recipetype/sub.png";
 import rice from "../../../public/recipetype/rice.png";
 import pasta from "../../../public/recipetype/pasta.png";
 import soup from "../../../public/recipetype/soup.png";
+import logo from "../../../public/logo.png";
 import RecipeTypeItem from "@/app/(afterWellcome)/_components/RecipeTypeItem";
 import SearchArea from "@/app/(afterWellcome)/_components/SearchArea";
 import Image from "next/image";
-
+import Profile from "@/app/(afterWellcome)/_components/Profile";
+type recipeTypeItem ={
+    url:string,
+    imageSrc:File,
+    label:string
+}
+const recipeTypeItems:recipeTypeItem[] = [
+    {url:'/home',imageSrc:all,label:'전체'},
+    {url:'/main',imageSrc:main,label:'메인반찬'},
+    {url:'/sub',imageSrc:sub,label:'밑반찬'},
+    {url:'/soup',imageSrc:soup,label:'찌개/국'},
+    {url:'/rice',imageSrc:rice,label:'밥'},
+    {url:'/pasta',imageSrc:pasta,label:'면'},
+]
 export default function Layout({children,}: { children: React.ReactNode }) {
     return (
         <div className={styles.container}>
-            <header className={styles.topSectionWrapper}>
-                <section className={styles.topSection}>
-                    <div className={styles.topSectionWhiteArea}>
-                        <Image className={styles.logo} src={soup} priority={'empty'}/>
-                        <div className={styles.topSectionFixed}>
-                           <SearchArea/>
-                            <div className={styles.recipeTypeWrapper}>
-                                <RecipeTypeItem url={'/'} imageSrc={all} label={'전체'}/>
-                                <RecipeTypeItem url={'/'} imageSrc={main} label={'메인반찬'}/>
-                                <RecipeTypeItem url={'/'} imageSrc={sub} label={'밑반찬'}/>
-                                <RecipeTypeItem url={'/'} imageSrc={soup} label={'찌개/국'}/>
-                                <RecipeTypeItem url={'/'} imageSrc={rice} label={'밥'}/>
-                                <RecipeTypeItem url={'/'} imageSrc={pasta} label={'면'}/>
-                            </div>
-                        </div>
+            <header className={styles.topSection}>
+                <div className={styles.logoWrapper}>
+                    <Image className={styles.logo} src={logo} priority={'empty'} alt={'logo'}/>
+                </div>
+                <div className={styles.topSearchSection}>
+                    <SearchArea/>
+                    <div className={styles.recipeTypeWrapper}>
+                        {recipeTypeItems.map((item)=> <RecipeTypeItem item={item} key={item.url}/>)}
                     </div>
-                </section>
+                </div>
+                <Profile/>
             </header>
             <div className={styles.bottomSectionWrapper}>
                 {children}
