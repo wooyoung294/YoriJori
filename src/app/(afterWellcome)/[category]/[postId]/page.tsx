d35ed7c26e83e0741a4f3da8a64cbe4d523a2928
wooyoung <andy241822@gmail.com>
2024-03-12 01:30:25 +0900

    [Feat] 레시피 상세보기 페이지 제작

---
+++ src/app/(afterWellcome)/[category]/[postId]/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from "react";
+import styled from './page.module.css'
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { getRecipeDetail } from "@/app/api/recipe";
+export default function RecipeDetail() {
+    const params = useParams();
+    const {postId} = params;
+    const { data, isLoading, isError} =
+        useQuery(
+            { queryKey: ["recipe", "detail"],
+                queryFn: () => getRecipeDetail(postId as string) }
+        );
+    if (isError) {
+        return "실패";
+    }
+    if (isLoading) {
+        return "로딩중";
+    }
+    return (
+        <div className={styled.recipeDetailWrapper}>
+            <div className={styled.imgWrapper}>
+                <Image src={`data:image/jpeg;base64,${data.imageSrc}`}
+                       alt={'테스트'}
+                       fill
+                />
+            </div>
+            <div className={styled.textWrapper}>
+                <h2> {data.title} </h2>
+                <br/>
+                <pre className={styled.recipeStep}>
+                    <h3> 조리방법 </h3>
+                    {data.description}
+                </pre>
+            </div>
+        </div>
+    );
+}
