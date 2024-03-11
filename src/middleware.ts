c94de2b59b96153e52845f30e6973aa5bf45c017
wooyoung <andy241822@gmail.com>
2024-03-12 01:41:26 +0900

    [Feat] 비로그인시 레시피를 작성할 수 없어 Url 접근시 login page로 돌려보냄

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from "./auth"
+import {NextResponse} from "next/server";
+
+export async function middleware() {
+    const session = await auth();
+    console.log(session);
+    if (!session) {
+        return NextResponse.redirect('http://localhost:3000');
+    }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ['/createRecipe'],
+}
