fa956cc3cb79f1cbf58ab0b9d13917293d74d91f
wooyoung <andy241822@gmail.com>
2024-03-13 03:50:45 +0900

    [Refact] 검색시 글자를 타이핑 할때마다 검색이 되서 수정

--- src/app/(afterWellcome)/_store/zustandStore.ts
+++ src/app/(afterWellcome)/_store/zustandStore.ts
@@ -2,19 +2,24 @@ import { create } from "zustand";
 import React from "react";
 type searchTextStoreType = {
     searchText:string,
-    onChangeSearchText: React.ChangeEventHandler<HTMLInputElement>,
+    onChangeSearchText: Function,
     order:"recommend" | "new" | "view",
     onChangeOrder:Function
     setInitValue: Function,
+    searchInputValue:string,
+    onChangeSearchInputValue: Function,
 }
 export const useSearchTextStore = create<searchTextStoreType>((set)=>({
     searchText: '',
-    onChangeSearchText: (e:React.ChangeEvent<HTMLInputElement>)=>
-        set(() => ({searchText:e.currentTarget.value})),
+    onChangeSearchText:(value:string)=>
+        set(()=>({searchText:value})),
     order:"recommend",
     onChangeOrder:(value:"recommend" | "new" | "view")=>
         set(() => ({order:value})),
     setInitValue:()=>{
-        set(()=>({order:"recommend",searchText:''}))
-    }
+        set(()=>({order:"recommend",searchText:'',searchInputValue:''}))
+    },
+    searchInputValue:'',
+    onChangeSearchInputValue:(e:React.ChangeEvent<HTMLInputElement>)=>
+        set(()=>({searchInputValue:e.target.value})),
 }))
