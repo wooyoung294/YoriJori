f03ff7ec12fd57818b199bc4801a2229ecf542c1
wooyoung <andy241822@gmail.com>
2024-03-13 03:50:06 +0900

    [Refact] 글자를 타이핑 할때마다 검색이 되서 수정

--- src/app/(afterWellcome)/_components/SearchArea.tsx
+++ src/app/(afterWellcome)/_components/SearchArea.tsx
@@ -3,34 +3,35 @@ import React, { useRef } from "react";
 import styles from "@/app/(afterWellcome)/_components/searchArea.module.css";
 import { useSearchTextStore } from "@/app/(afterWellcome)/_store/zustandStore";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function SearchArea() {
 
     const {
-        searchText,
+        searchInputValue,
+        onChangeSearchInputValue,
         onChangeSearchText
     } = useSearchTextStore();
-
-    const searchTextInput = useRef<HTMLInputElement>(null);
     const router = useRouter();
+    const searchTextInput = useRef<HTMLInputElement>(null);
     const onClick: React.MouseEventHandler<HTMLButtonElement> = () => {
-        if (!searchText) {
+        if (!searchTextInput.current?.value) {
             alert("검색어를 입렵해주세요");
             searchTextInput.current?.focus();
         } else {
-            router.push(`/search?searchText=${searchText}&order=recommend`);
+            onChangeSearchText(searchTextInput.current?.value)
         }
 
     };
 
     return (
-        <div className={styles.searchWrapper}>
+        <div  className={styles.searchWrapper}>
             <input className={styles.searchInput}
                    name="searchText"
                    type="text"
+                   value={searchInputValue}
+                   onChange={(e)=>onChangeSearchInputValue(e)}
                    ref={searchTextInput}
-                   value={searchText}
-                   onChange={(e) => onChangeSearchText(e)}
             />
             <button className={styles.searchBtn} onClick={onClick}>검색</button>
         </div>
