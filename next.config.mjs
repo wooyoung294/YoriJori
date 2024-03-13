41f869d61661b779ea8529d445a9445222df6784
wooyoung <andy241822@gmail.com>
2024-03-13 19:22:00 +0900

    [Refact] MSW사용시 필요한 images.domain 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images: {
-        domains: [
-            "loremflickr.com",
-            "picsum.photos"
-            // ...
-        //
-        ]
-    }
 };
 
 export default nextConfig;
